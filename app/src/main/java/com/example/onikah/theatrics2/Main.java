69284548f71522a3735fd80e7f65d0a18179be1d
Onikah <mona.kwamboka@gmail.com>
2016-03-09 09:53:05 +0300

    redirect to info ripple effect

--- app/src/main/java/com/example/onikah/theatrics2/Main.java
+++ app/src/main/java/com/example/onikah/theatrics2/Main.java
@@ -1,17 +1,71 @@
+
 package com.example.onikah.theatrics2;
 
-import android.os.Bundle;
-import android.support.v4.app.BundleCompat;
-import android.support.v7.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+
 
 /**
  * Created by onikah on 3/8/16.
  */
 public class Main extends AppCompatActivity {
+    public boolean isFirstStart;
+
     @Override
-    public void onCreate(Bundle savedIntanceState){
+    public void onCreate(Bundle savedIntanceState) {
         super.onCreate(savedIntanceState);
         setContentView(R.layout.home);
+        //  Declare a new thread to do a preference check
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    Intent i = new Intent(Main .this, MainActivity.class);
+                    startActivity(i);
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+
+        // Start the thread
+        t.start();
+
+
+    }
+
+    public void onButtonClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this,Info_Activity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+
+
+        }
+
     }
 }
-
